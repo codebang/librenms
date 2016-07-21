4ce0816017705850778437d90d83e07d4cb6ad1e
root <haoyan@tethrnet.com>
2016-07-21 01:49:20 +0000

    fix bugs

--- html/plugins/Feature/Feature.inc.php
+++ html/plugins/Feature/Feature.inc.php
@@ -1,3 +1,15 @@
-<?php
-
-echo 'Well done, the plugin system is up and running';
+<h4> Information about TNMS </h4></br>
+This version only provides basic switch and port management and limited integration with DMS system<br>
+But TNMS can do more, it is a full functional network management system which can support:<br>
+<div>
+<ul>
+  <li>support auto-discovery</li>
+  <li>support topology discovery</li>
+  <li>support bgp, ospf device poll</li>
+  <li>support alarm customization / template</li>
+  <li>support dashbard customization</li>
+  <li>support service/app management and monitor</li>
+  <li>support plugin development</li>
+</ul>
+</div>
+If you want to try these amazing funcitons, can contact <a href='http://www.tethrnet.com/'>Tethrnet</a>
